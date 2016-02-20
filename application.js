c162275b6e3fae25b2f58e6d4d7f39e4d8c68592
Lakshmi Kotteda <lakshmikotteda@hotmail.com>
2016-02-20 06:41:23 +0000

    Added and styled canvas

---
+++ application.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+  var canvas = document.getElementById("canvas");
+  var shape = canvas.getContext("2d");
+
+  shape.fillStyle = "white";
+  shape.fillRect(10, 10, 50, 50);
+
+});

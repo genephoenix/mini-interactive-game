35d19a9305d05b5056c49d5ea385b7a4f0d29f52
Lakshmi Kotteda <lakshmikotteda@hotmail.com>
2016-02-21 01:05:20 +0000

    Removed canvas

--- application.js
+++ application.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
-  var canvas = document.getElementById("canvas");
-  var shape = canvas.getContext("2d");
+  // var canvas = document.getElementById("canvas");
+  // var shape = canvas.getContext("2d");
 
-  shape.fillStyle = "white";
-  shape.fillRect(10, 10, 50, 50);
+  // shape.fillStyle = "white";
+  // shape.fillRect(10, 10, 50, 50);
 
 });
